12d6e2354b61a9c6f5f8bd4111b0f4cbde55326d
ekohunterz <ekohunterz@gmail.com>
2023-10-16 14:25:58 +0700

    fix

--- src/components/SurahCard.vue
+++ src/components/SurahCard.vue
@@ -1,15 +1,10 @@
 <template>
-  <router-link
-    :to="{ name: 'surah', params: { nomor: props.nomor } }"
-    class="border-2 border-gray-300 p-4 rounded-lg hover:border-primary text-gray-500 dark:text-secondary dark:hover:text-primary hover:text-primary"
-  >
+  <router-link :to="{ name: 'surah', params: { nomor: props.nomor } }" class="border-2 border-gray-300 p-4 rounded-lg hover:border-primary text-gray-500 dark:text-secondary dark:hover:text-primary hover:text-primary">
     <div class="flex justify-between">
       <div class="flex justify-center items-center">
         <div class="relative mr-4">
           <IconStar />
-          <div
-            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-bold text-sm"
-          >
+          <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-bold text-sm">
             {{ props.nomor }}
           </div>
         </div>
@@ -26,7 +21,7 @@
   </router-link>
 </template>
 <script setup>
-import IconStar from './Icons/IconStar.vue'
+import IconStar from '@/Icons/IconStar.vue'
 
 const props = defineProps({
   nomor: Number,
